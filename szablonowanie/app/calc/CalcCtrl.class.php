e4ae4b38b1c17df92d410643678f13380c943f2b
Veeliz <146839485+Veeliz@users.noreply.github.com>
2024-06-17 13:34:29 +0200

    Add files via upload

---
+++ szablonowanie/app/calc/CalcCtrl.class.php
@@ -0,0 +1,123 @@
+<?php
+// W skrypcie definicji kontrolera nie trzeba dołączać problematycznego skryptu config.php,
+// ponieważ będzie on użyty w miejscach, gdzie config.php zostanie już wywołany.
+
+require_once $conf->root_path.'/lib/smarty/Smarty.class.php';
+require_once $conf->root_path.'/lib/Messages.class.php';
+require_once $conf->root_path.'/app/calc/CalcForm.class.php';
+require_once $conf->root_path.'/app/calc/CalcResult.class.php';
+
+/** Kontroler kalkulatora
+ * @author Przemysław Kudłacik
+ *
+ */
+class CalcCtrl {
+
+	private $msgs;   //wiadomości dla widoku
+	private $infos;  //informacje dla widoku
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+	private $hide_intro; //zmienna informująca o tym czy schować intro
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->msgs = new Messages();
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+		$this->hide_intro = false;
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->kwota = isset($_REQUEST ['kwota']) ? $_REQUEST ['kwota'] : null;
+		$this->form->lata = isset($_REQUEST ['lata']) ? $_REQUEST ['lata'] : null;
+		$this->form->oprocentowanie = isset($_REQUEST ['oprocentowanie']) ? $_REQUEST ['oprocentowanie'] : null;
+
+		var_dump($this->form);
+	}
+	
+	//walidacja parametrów z przygotowaniem zmiennych dla widoku
+	function validate(){
+	
+		//sprawdzenie, czy parametry zostały przekazane - jeśli nie to zakończ walidację
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->lata ) && isset ( $this->form->oprocentowanie))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false; //zakończ walidację z błędem
+		} else { 
+			$this->hide_intro = true; //przyszły pola formularza, więc - schowaj wstęp
+		}
+	
+		if ($this->form->kwota == "") {
+			$this->msgs->addError('Nie podano liczby kwoty');
+		}
+		if ($this->form->lata == "") {
+			$this->msgs->addError('Nie podano liczby lat');
+		}
+		if ($this->form->oprocentowanie == "") {
+			$this->msgs->addError('Nie podano liczby oprocentowania');
+		}
+
+		//nie ma sensu walidować dalej gdy brak parametrów
+
+		if (! $this->msgs->isError()) {
+			
+			// sprawdzenie, czy $x i $y są liczbami całkowitymi
+			if (! is_numeric ( $this->form->kwota )) {
+				$this->msgs->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->lata )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+			if (! is_numeric ( $this->form->oprocentowanie )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! $this->msgs->isError();
+	}
+		
+	// wykonaj obliczenia
+	function process(){
+		$this->getparams();
+
+		
+		if ($this->validate()) {
+		//konwersja parametrów na int
+		$this->form->kwota = floatval($this->form->kwota);
+		$this->form->lata = floatval($this->form->lata);
+		$this->form->oprocentowanie = floatval($this->form->oprocentowanie);
+		
+		//wykonanie operacji
+		$oplatamsc = ($this->form->kwota *  ($this->form->oprocentowanie / 100 / 12))/ (1 - pow(1 + ($this->form->oprocentowanie / 100 / 12), -($this->form->lata * 12)));
+		$result = round($oplatamsc);
+
+		$this->msgs->addInfo('Wykonano obliczenia.');
+	}
+	$this->generateView();
+}
+	
+	public function generateView(){
+		global $conf;
+		
+		$smarty = new Smarty();
+		$smarty->assign('conf',$conf);
+		
+		$smarty->assign('page_title','Kalkulator kredytowy');
+		$smarty->assign('page_description','Obiektowość. Funkcjonalność aplikacji zamknięta w metodach różnych obiektów. Pełen model MVC.');
+		$smarty->assign('page_header','Obiekty w PHP');
+				
+		$smarty->assign('hide_intro',$this->hide_intro);
+		
+		$smarty->assign('msgs',$this->msgs);
+		$smarty->assign('form',$this->form);
+		$smarty->assign('res',$this->result);
+		
+		$smarty->display($conf->root_path.'/app/calc/CalcView.html');
+	}
+}
